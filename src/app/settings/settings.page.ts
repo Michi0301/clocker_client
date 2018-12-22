adc12b8196c6e9e5317bacf694ca1659a1f84010
Michael <1938084+Michi0301@users.noreply.github.com>
2018-12-22 20:19:15 +0100

    Adds basic user credentials form.

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['settings.page.scss'],
 })
 export class SettingsPage {
+  private user_credentials: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.user_credentials = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  logForm() {
+    console.log(this.user_credentials.value)
+  }
 }
