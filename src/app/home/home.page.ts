f1a2dd5976559b60135e43033ef4467c6d067d50
Michael <1938084+Michi0301@users.noreply.github.com>
2018-12-26 18:51:32 +0100

    Binds logic to buttons.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,15 +11,43 @@ import { ClockState } from '../models/clock-state';
 export class HomePage implements OnInit {
   clockState: ClockState;
 
-  constructor(private clocker_client: ClockerClient) {
-    this.clockState = new ClockState('loading');
-  };
+  constructor(private clocker_client: ClockerClient) {};
 
   ngOnInit() {
+    this.fetchCurrent();
+  }
+
+  fetchCurrent() {
+    this.clockState = new ClockState('loading');
+
     this.clocker_client.getCurrentState().subscribe((clockState) => {
       this.clockState = clockState;
 
       console.log(clockState);
+    });
+  }
+
+  clockIn() {
+    this.clocker_client.clockIn().subscribe((result) => {
+      
+      this.fetchCurrent();
+      console.log(result);
+    });
+  }
+
+  clockOut() {
+    this.clocker_client.clockOut().subscribe((result) => {
+
+      this.fetchCurrent();
+      console.log(result);
+    });
+  }
+
+  clockPause() {
+    this.clocker_client.clockPause().subscribe((result) => {
+
+      this.fetchCurrent();
+      console.log(result);
     });
   }
 }
--- src/app/models/clocker-client.ts
+++ src/app/models/clocker-client.ts
@@ -15,5 +15,17 @@ export class ClockerClient {
   getCurrentState() {
     return this.http.post<ClockState>(Settings.get('server') + "/current", ClockerClient.credentials)
   }
+
+  clockIn() {
+    return this.http.post<ClockState>(Settings.get('server') + "/clockin", ClockerClient.credentials)
+  }
+
+  clockOut() {
+    return this.http.post<ClockState>(Settings.get('server') + "/clockout", ClockerClient.credentials)
+  }
+
+  clockPause() {
+    return this.http.post<ClockState>(Settings.get('server') + "/pause", ClockerClient.credentials)
+  }
 }
 
