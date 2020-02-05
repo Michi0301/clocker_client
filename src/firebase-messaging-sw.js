76fcec3401043eb4e5ed258b1a7d07e3801b193a
Michi0301 <1938084+Michi0301@users.noreply.github.com>
2020-02-05 14:20:47 +0100

    Updates firebase dependencies.

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,14 +1,22 @@
 // See https://ionicthemes.com/tutorials/about/the-complete-guide-to-progressive-web-apps-with-ionic4
 
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.8.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.8.0/firebase-messaging.js');
 
-const PUBLIC_VAPID_KEY = 'BNuqePVbeKJA4JUkzMPq0X1qgc11noBm6CYeTwetQ3BMcv3d2GH-XzB-96X1_0S5BkwS8RtjIC93LI88MtiCcLA';
-const SENDER_ID = '282924589880';
+const FIREBASE_PUBLIC_VAPID_KEY = 'BNuqePVbeKJA4JUkzMPq0X1qgc11noBm6CYeTwetQ3BMcv3d2GH-XzB-96X1_0S5BkwS8RtjIC93LI88MtiCcLA';
 
-firebase.initializeApp({
-  'messagingSenderId': SENDER_ID
-});
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyArBlGG1yhkIPUC6DxWwpaMhZh7ojo-2Y8",
+  authDomain: "clocker-client.firebaseapp.com",
+  databaseURL: "https://clocker-client.firebaseio.com",
+  projectId: "clocker-client",
+  storageBucket: "clocker-client.appspot.com",
+  messagingSenderId: "282924589880",
+  appId: "1:282924589880:web:fd483d8810a854567817cd"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
-messaging.usePublicVapidKey(PUBLIC_VAPID_KEY);
+messaging.usePublicVapidKey(FIREBASE_PUBLIC_VAPID_KEY);
